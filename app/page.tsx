d1aabf37c6c440f6a7c1e0f6818965f6b53e4962
NSOLAf1 <nsolaf1@gmail.com>
2024-11-14 15:42:20 +0600

    upt

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Clone from "@/components/Clone";
 import TodoistClone from "@/components/TodoistClone";
 
 export default function Home() {
   return (
     <div>
       <TodoistClone />
+        3x Comp(Items, List, FORM)
+      {/* <Clone/> */}
     </div>
   );
 }
--- components/TodoistClone.tsx
+++ components/TodoistClone.tsx
@@ -29,6 +29,7 @@ export default function Todo() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-lg">
       <h1 className="text-2xl font-bold mb-4 text-gray-800">Todoist Clone</h1>
